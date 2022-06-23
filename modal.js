5711791720681144fab485fc576788e4e1311053
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2022-06-23 04:43:20 -0300

    display tasks

--- modal.js
+++ modal.js
@@ -1,3 +1,4 @@
+// Frase do dia
 function iniciaModalFrase(modalContainerID) {
     const modalContainer = document.getElementById(modalContainerID);
     if (modalContainer) {
@@ -14,7 +15,7 @@ const containerFraseDoDia = document.querySelector('.containerFraseDoDia');
 containerFraseDoDia.addEventListener('click', () => iniciaModalFrase('modal-frase'));
 
 
-
+// Adicionar nova task
 function iniciaModalAdd(modalContainerID) {
     const modalContainer = document.getElementById(modalContainerID);
     if (modalContainer) {
@@ -29,3 +30,20 @@ function iniciaModalAdd(modalContainerID) {
 
 const addButton = document.querySelector('.addButton');
 addButton.addEventListener('click', () => iniciaModalAdd('modal-add'));
+
+
+// Descricao
+const coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}

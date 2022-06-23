06170e2e2d6df970fe2082cebe0537979e0bb575
Maria Clara Fadel <mariaclarapereira@alunos.utfpr.edu.br>
2022-06-23 06:32:19 -0300

    validacao erro

---
+++ validacao.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('form')
+const formTitulo = document.getElementById('formTitulo')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    checkInputs()
+})
+
+function checkInputs() {
+    const formTituloValue = formTitulo.value.trim()
+    
+
+    if (formTituloValue === '') {
+        setErrorFor(formTitulo)
+    } else {
+        succesValidation(formTitulo)
+    }
+}
+
+function setErrorFor(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control error'
+}
+
+function succesValidation(input) {
+    const formControl = input.parentElement;
+
+    formControl.className = 'form-control'
+}
